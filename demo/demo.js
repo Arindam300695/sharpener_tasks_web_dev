cf259986b225b80ab51df843b4a8d06822c97ec6
Arindam <ari.chat1995@gmail.com>
2023-03-15 20:16:32 +0530

    first commit

---
+++ demo/demo.js
@@ -0,0 +1,7 @@
+const titleRef = document.querySelector("title");
+const headerTitle = document.querySelector("#header-title");
+const addItemRef = document.querySelector(".title");
+titleRef.innerText = "Modified Item Listener";
+headerTitle.style.borderBottom = "3px solid black";
+addItemRef.style.fontWeight = "bold";
+addItemRef.style.color = "green";
