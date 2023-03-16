6cc6fe63e16590834d24ac317b5d1b4db9d1b393
Arindam <ari.chat1995@gmail.com>
2023-03-16 13:38:35 +0530

    tenth commit

--- demo/form.js
+++ demo/form.js
@@ -4,12 +4,19 @@ function myFunction() {
   let phoneValue = document.getElementById("Phone").value;
   let timeValue = document.getElementById("Time").value;
   let dateValue = document.getElementById("Date").value;
+  //   creating object of the values entered by the user
+  let obj = {
+    name: nameValue,
+    email: emailValue,
+    phone: phoneValue,
+    time: timeValue,
+    date: dateValue,
+  };
+  // converting the object to string in order to store it in the local storage
+  const string = JSON.stringify(obj);
   // storing the values in the local storage
-  localStorage.setItem("Name", nameValue);
-  localStorage.setItem("Email", emailValue);
-  localStorage.setItem("Phone", phoneValue);
-  localStorage.setItem("Time", timeValue);
-  localStorage.setItem("Date", dateValue);
+  localStorage.setItem("data", string);
+  // making the input fields empty each time user will click the submit button
   nameValue = "";
   emailValue = "";
   phoneValue = "";
