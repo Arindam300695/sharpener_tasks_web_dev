9f5a9218489b996ab678ed1d964e8348270f9523
Arindam Chattopadhyay <arindam.chattopadhyay300695@gmail.com>
2023-03-23 11:54:08 +0530

    solution of too many promises, 'sharpener'

---
+++ demo/promise.js
@@ -0,0 +1,56 @@
+const posts = [];
+let count = 0;
+//Do not touch this function
+function createPost(callBack) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      posts.push({ title: `POST${count}` });
+      const post = posts[count].title;
+      resolve(
+        post,
+        callBack().then((currentTime) => {
+          console.log(currentTime);
+        })
+      );
+      count++;
+    }, 1000);
+  });
+}
+
+//Do not touch this function
+function deletePost() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (posts.length > 0) {
+        const poppedElement = posts.pop();
+        resolve(poppedElement);
+      } else {
+        reject("ERROR: ARRAY IS EMPTY");
+      }
+    }, 2000);
+  });
+}
+
+//update user actiovity
+function updateLastUserActivityTime() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const currentTime = new Date().getTime();
+      resolve(currentTime);
+    }, 1000);
+  });
+}
+
+Promise.all([
+  createPost(updateLastUserActivityTime),
+  createPost(updateLastUserActivityTime),
+  createPost(updateLastUserActivityTime),
+  createPost(updateLastUserActivityTime),
+  deletePost(),
+])
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
